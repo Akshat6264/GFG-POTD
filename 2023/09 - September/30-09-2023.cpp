e4858d7e2408bcd5c52af8c08dbf322489043626
Akshat6264 <akshatkelotra@gmail.com>
2023-09-30 23:25:59 +0530

    Boolean Matrix

---
+++ 2023/09 - September/30-09-2023.cpp
@@ -0,0 +1,91 @@
+/*
+
+Author : Akshat Kelotra
+Date : 30/09/2023
+Problem : Boolean Matrix
+Difficulty : Medium
+Problem Link : https://practice.geeksforgeeks.org/problems/boolean-matrix-problem-1587115620/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    void booleanMatrix(vector<vector<int> > &matrix)
+    {
+        int n=matrix.size(),m=matrix[0].size();
+        
+        bool rows[n];
+        bool cols[m];
+        
+        for(int i=0;i<n;i++) rows[i]=false;
+        for(int j=0;j<m;j++) cols[j]=false;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==1)
+                {
+                    rows[i]=true; cols[j]=true;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(rows[i] or cols[j]) matrix[i][j]=1;
+            }
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        ob.booleanMatrix(matrix);
+
+
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends

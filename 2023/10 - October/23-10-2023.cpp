ef5e0ec39189e677595fd7f43753a7e2e1016a93
Akshat6264 <akshatkelotra@gmail.com>
2023-10-23 23:13:18 +0530

    Maximum sum increasing subsequence

---
+++ 2023/10 - October/23-10-2023.cpp
@@ -0,0 +1,63 @@
+/*
+
+Author : Akshat Kelotra
+Date : 23/10/2023
+Problem : Maximum sum increasing subsequence
+Difficulty : Medium
+Problem Link : https://practice.geeksforgeeks.org/problems/maximum-sum-increasing-subsequence4749/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+
+	public:
+	int maxSumIS(int arr[], int n)  
+	{  
+	    vector<int> dp(arr, arr + n);
+	    int ans = dp[0];
+	    for(int i = 1; i < n; i++){
+	        for(int j = i - 1; j >= 0; j--){
+	            if(arr[j] < arr[i]){
+	                dp[i] = max(dp[i], dp[j] + arr[i]);
+	            }
+	        }
+	        ans = max(ans, dp[i]);
+	    }
+	    return ans;
+	}  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+      
+
+	    Solution ob;
+	    cout << ob.maxSumIS(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends

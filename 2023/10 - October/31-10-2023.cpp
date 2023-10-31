9d82463811a6a32e04318e721a8e0bbfceafa096
Akshat6264 <akshatkelotra@gmail.com>
2023-10-31 22:47:45 +0530

    Move all zeroes to end of array

---
+++ 2023/10 - October/31-10-2023.cpp
@@ -0,0 +1,50 @@
+/*
+
+Author : Akshat Kelotra
+Date : 31/10/2023
+Problem : Move all zeroes to end of array
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/move-all-zeroes-to-end-of-array0751/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    int i=0, j=0;
+        while(j<n) {
+            if(arr[j] != 0) {
+                swap(arr[i++], arr[j]);
+            }
+            j++;
+        }
+	}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.pushZerosToEnd(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

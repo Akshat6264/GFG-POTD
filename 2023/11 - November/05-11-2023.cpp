4e5d1279593b23f4551f2e1a26d3c79f12783c4f
Akshat6264 <akshatkelotra@gmail.com>
2023-11-05 23:40:00 +0530

    Top K Frequent Elements in Array - |

---
+++ 2023/11 - November/05-11-2023.cpp
@@ -0,0 +1,55 @@
+/*
+
+Author : Akshat Kelotra
+Date : 05/11/2023
+Problem : Top K Frequent Elements in Array - |
+Difficulty : Easy
+Problem Link : https://www.geeksforgeeks.org/problems/top-k-frequent-elements-in-array/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> topK(vector<int>& nums, int k) {
+        vector <int> ans;
+        priority_queue <pair <int, int>> pq;
+        unordered_map <int, int> mp;
+        for (auto it : nums){
+            mp[it]++;
+        }
+        for (auto it : mp){
+            pq.push({it.second, it.first});
+        }
+        for (int i=0; i<k; i++){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<int> nums(n);
+        for (auto &i : nums) cin >> i;
+        int k;
+        cin >> k;
+        Solution obj;
+        vector<int> ans = obj.topK(nums, k);
+        for (auto i : ans) cout << i << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

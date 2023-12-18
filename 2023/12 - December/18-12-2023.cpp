0cd215f552f025969b87913a435d5b5895a9f890
Akshat6264 <akshatkelotra@gmail.com>
2023-12-18 22:20:38 +0530

    Game of XOR

---
+++ 2023/12 - December/18-12-2023.cpp
@@ -0,0 +1,48 @@
+/*
+
+Author : Akshat Kelotra
+Date : 18/12/2023
+Problem : Game of XOR
+Difficulty : Medium
+Problem Link : https://www.geeksforgeeks.org/problems/game-of-xor1541/1
+
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int gameOfXor(int N , int A[]) {
+        int ans = 0;
+        
+        for(int i = 0; i < N; i++){
+            if(((i + 1) & 1) and ((N - i) & 1)){
+                ans ^= A[i];
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.gameOfXor(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
